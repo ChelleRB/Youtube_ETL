c12ba5d44a138b51c3e4367f52a8696ed60a6022
ChelleRB <Rochellebrandy@icloud.com>
2026-02-24 19:33:02 +0000

    added a data folder to save all the json data collected

--- video_stats.py
+++ video_stats.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from datetime import date
 
 import os #needed for dotenv file which has the API key
 from dotenv import load_dotenv
@@ -115,12 +116,19 @@ def extract_video_data(video_ids):
   except requests.exceptions.RequestException as e:
       raise e
 
+def save_to_json(extracted_data):
+    file_path = f"./data/Youtube_data_{date.today()}.json"
+
+    with open(file_path, "w", encoding="utf-8") as json_outfile:
+       json.dump(extracted_data, json_outfile, indent=4, ensure_ascii=False) #indent will make json file more readable
+
 
 if __name__ == "__main__":
   #print("get playlist_id will be executed")
   playlistId = get_playlist_id()
   video_ids = get_video_ids(playlistId) #to test remember to use print
-  extract_video_data(video_ids)
+  video_data = extract_video_data(video_ids)
+  save_to_json(video_data)
 
 # STEP 1 playlist ID(channels) 
 # STEP 2 goes to.. VIDEO ID(playlistId items) 
